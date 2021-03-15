b0f212ebee9d0aad6f8f8aaefed4a4607f241003
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-03-15 15:52:37 -0600

    Created post function and gets data from file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+require('dotenv').config();
 var app = express();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,4 +18,15 @@ app.get('/', function (req, res) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  let filename = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({
+    name: filename,
+    type: type,
+    size: size
+  });
 });
